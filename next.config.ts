bd84d362973367384614c58b834c8aa43827ea83
fujiwara-kotomi <kotofuji3@gmail.com>
2026-08-24 17:01:54 +0900

    通知機能

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,25 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   turbopack: {root:process.cwd(),},
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  turbopack: {root:process.cwd(),},
-};
-
-export default nextConfig;
+const withPWA = withPWAInit({ 
+  dest: "public",
+  // NotificationInitializer 側で手動登録するため自動登録はしない
+  register: false,
+  // next-pwa が生成する Service Worker の名前
+  sw: "firebase-messaging-sw.js",
+  // 今回はPCでService Workerを確認したいので開発環境でも有効
+  disable: false,
+});
+const nextConfig: NextConfig = {};
+export default withPWA(nextConfig);
 
 
